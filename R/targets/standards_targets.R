e8c989dccd939a645d8352286c5cbc9918899c0d
jasenfinch <jsf9@aber.ac.uk>
2023-11-09 09:39:21 +0000

    Update the assignment parameters in the methods section

--- R/targets/standards_targets.R
+++ R/targets/standards_targets.R
@@ -1,10 +1,12 @@
 
+search_ppm <- 4 
+
 assignment_parameters <- {
   p <- assignments::assignmentParameters("FIE-HRMS") 
   assignments::adducts(p)$n <- c("[M-H]1-","[M+Cl]1-","[M+Cl37]1-","[M+K-2H]1-","[M-2H]2-","[2M-H]1-")
   assignments::adducts(p)$p <- c("[M+Na]1+","[M+H]1+","[M+K]1+","[M+K41]1+","[M+2H]2+","[2M+H]1+")
   assignments::isotopes(p) <- c("13C","18O","34S")
-  assignments::ppm(p) <- 4
+  assignments::ppm(p) <- search_ppm
   p
 }
 
